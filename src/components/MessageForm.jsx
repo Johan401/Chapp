0360bfb30b49e72950b434d9228e825511b5ec00
Johan Zamora <61443192+Johan401@users.noreply.github.com>
2023-05-26 18:31:06 -0600

    Add files via upload

---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,157 @@
+import React,{useContext} from "react";
+import { Attachment } from "./svg/Attachment";
+import { AudioRecord } from "./AudioRecord";
+import { Send } from "./svg/Send";
+import {appContext} from "../context/AppContext.js";
+import axios from "axios";
+
+const MessageForm = ({ handleSubmit, text, setText, setFile, file }) => {
+
+const {openDialog,location,findMyLocation,handleOpenWeatherCommant, weather,setSelectedGIF,handleSearchGIFSelected, setOpenDialog,TimeLocation, reminder,handleOpenWeather, setReminder} = useContext(appContext)
+
+const verifyCommands = () => {
+  const haveNumber = stringContainsNumber(text)
+  const wordCount = WordCount(text)
+  if(text.includes("/searchGIF")){
+    const arrayWords = text.split(" ")
+    handleSearchGIFSelected(arrayWords[1])
+  }if(text.includes("/wikipedia")){
+    const arrayWords = text.split(" ")
+    console.log(arrayWords[1])
+    const endpoint = 'https://en.wikipedia.org/w/api.php?';
+    const params = {
+        origin: '*',
+        format: 'json',
+        action: 'query',
+        prop: 'extracts',
+        exchars: 250,
+        exintro: true,
+        explaintext: true,
+        generator: 'search',
+        gsrlimit: 20,
+    };
+    params.gsrsearch = arrayWords[1]
+    axios.get(endpoint, { params }).then((response) => {
+      let searchObject = response.data.query.pages
+      console.log(response.data.query)
+      let searchId = searchObject[Object.keys(searchObject)[0]]
+      console.log(searchId)
+      window.open(`https://en.wikipedia.org/?curid=${searchId.pageid}`)
+    });
+
+  }if(text.includes("/myLocation")){
+    setText("La ubicacion actual es: "+location)
+  }if(text.includes("/showWeather")){
+    handleOpenWeatherCommant()
+    setText("El clima actual es: "+weather.main === undefined ? "Cloudy Rain" : weather.main)
+  }if(text.includes("/showHour")){
+    setText("La hora actual es: "+TimeLocation())
+  }if(text.includes("/createReminder")){
+    (openDialog.showReminder) ? setOpenDialog({...openDialog, showReminder: false}) :  setOpenDialog({...openDialog, showReminder: true})
+  }if(text.includes("/deleteReminder")){
+    setText(reminder.map((item) =>  `${item.idReminder} - ${item.nameReminder}`))
+    setTimeout(() => {
+      setText("To delete use: /delete number")
+    }, 4000);
+  }if(text.includes("/showReminder")){
+    (openDialog.showReminderTable) ? setOpenDialog({...openDialog, showReminderTable: false}) :  setOpenDialog({...openDialog, showReminderTable: true})
+  }if(text.includes("/modifyReminder")){
+    setText(reminder.map((item) =>  `${item.idReminder} - ${item.nameReminder}`))
+    setTimeout(() => {
+      setText("To update use: /update number")
+    }, 4000);
+  }if (text.includes("/delete") && haveNumber){
+    const idDelete = text.slice(-1)
+    const searchReminder = reminder.find(user => user.idReminder === parseInt(idDelete))
+    if(searchReminder !== undefined){
+        setReminder(reminder.filter(item=> item.idReminder !== searchReminder.idReminder))
+        setText("Success Delete")
+    }else{
+      setText("No exist")
+    } 
+  }if (text.includes("/update") & haveNumber & wordCount !== 4){
+    const idUpdate = text.slice(-1)
+    const searchReminder = reminder.find(user => user.idReminder === parseInt(idUpdate))
+    if(searchReminder !== undefined){
+        setText("nameReminder: "+searchReminder.nameReminder+" to: "+searchReminder.to+" date: "+searchReminder.date.date+" hour: "+searchReminder.date.hour+" message: "+searchReminder.message)
+        setTimeout(() => {
+          setText("To update use: /update number SelectAttribute NewAttribute")
+        }, 4000);
+      }else{
+      setText("No exist")
+    } 
+    //update 1 nameReminder Peda
+  }if (text.includes("/update") & haveNumber & wordCount === 4){
+    const arrayWords = text.split(" ")
+    const idRemider = arrayWords[1]
+    const selectRemider = arrayWords[2]
+    const newRemider = arrayWords[3]
+    const searchReminder = reminder.find(user => user.idReminder === parseInt(idRemider))
+    if(searchReminder !== undefined){
+      const propertyNames = Object.keys(searchReminder)
+      console.log(propertyNames)
+      if(propertyNames[1] === selectRemider){
+        searchReminder.nameReminder = newRemider
+      }if(propertyNames[2] === selectRemider){
+        searchReminder.to = newRemider
+      }if(propertyNames[3] === selectRemider){
+        searchReminder.date.date = newRemider
+      }if(propertyNames[5] === selectRemider){
+        searchReminder.date.hour = newRemider
+      }if(propertyNames[4] === selectRemider){
+        searchReminder.message = newRemider
+      }
+        setText("Success Update")
+    }else{
+       setText("No exist Reminder")
+    } 
+  }
+}
+
+function stringContainsNumber(_string) {
+  return /\d/.test(_string);
+}
+
+function WordCount(str) { 
+  return str.split(" ").length;
+}
+
+  return (
+    <div
+      className='absoluteBackground'
+    >
+      <form className="message_form" onSubmit={handleSubmit}>
+        <label htmlFor="file">
+          <Attachment />
+        </label>
+        <input
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+          type="file"
+          id="file"
+          accept="image/*, video/*, audio/*"
+          style={{ display: "none" }}
+        />
+        <div>
+          <input
+            type="text"
+            placeholder="Enter message"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            style={{ backgroundColor: "#f7f8fc" }}
+          />
+        </div>
+        <button
+        onClick={verifyCommands}
+          style={{ outline: "none", backgroundColor: "whiteSmoke", border: 0 }}
+        >
+          <Send colorActive={!file ? "#4d94ff" : "green"} />
+        </button>
+        <AudioRecord setFile={setFile} />
+      </form>
+    </div>
+  );
+};
+
+export { MessageForm };
---
+++ src/context/AppContext.js
@@ -0,0 +1,274 @@
+import React, { createContext, useState } from "react";
+import { doc, deleteDoc, setDoc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import CryptoJS from "crypto-js";
+import axios from "axios";
+
+const appContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+
+  //Dialog
+  const [openDialog, setOpenDialog] = useState({showGIF: false, showReminder: false, showReminderUpdate: false , showWeather: false, showReminderTable: false})
+  //Alerts
+  const [openAlert, setOpenAlert] = useState({showAlertCreate: false, showAlertUpdate: false})
+
+  //Usuarios bloqueados 
+  const [blockedUsers, setBlockedUsers] = useState([]);
+  
+  //variables para buscarMensaje 
+  const [textoBuscado, setTextoBuscado] = useState('');
+  const [textoBuscadoG, setTextoBuscadoG] = useState('');
+
+  //Reminder
+  const [reminder, setReminder] = useState([])
+
+  //Manejo de selección para editar o borrar mensaje
+  const [selectedMsg, setSelectedMsg] = useState("");
+  const [selected, setSelected] = useState(false);
+  const [msgs, setMsgs] = useState([]);
+  const [msgG, setMsgG] = useState([]);
+  const [Me, setMe] = useState();
+  
+  //Manejo de modals
+  const [openModal, setOpenModal] = useState({
+    modalDeleteMsg: false,
+    modalEditMsg: false,
+    modalCreateGroup: false,
+  });
+
+  const [data, setData] = useState({})
+  const [weather, setWeather] = useState([]) 
+  const [location, setLocation] = useState("")
+  const [selectGIF, setSelectGIF] = useState("")
+  const [selectedGIF, setSelectedGIF] = useState("")
+  const [GIF, setGIF] = useState([]);
+
+  const handleWeather = () =>{
+    findMyLocation()
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location},lat&APPID=d905ced1896df43dff776fec1fc81073`).then((response) => {
+        setData(response.data);
+        setWeather(response.data.weather[0])
+      });
+  }
+
+
+
+  const handleOpenWeatherCommant = () =>{
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location},lat&APPID=3bd740d64f5ad6b4786618ba189dbfb8`).then((response) => {
+        setData(response.data);
+        setWeather(response.data.weather[0])
+      });
+  }
+
+  const findMyLocation = () => {
+    const status = document.querySelector('.status')
+  
+  handleOpenWeatherCommant()
+  
+    const success = (position) => {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
+  
+      const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+  
+      fetch(geoApiUrl)
+      .then(res => res.json())
+      .then(data => {
+        let locationRepalce = data.locality.replace(/Costa Rica/g, '')
+        locationRepalce = locationRepalce.replace(/\(/g, '');
+        locationRepalce = locationRepalce.replace(/\)/g, '');
+        
+        setLocation(locationRepalce)
+      })
+    }
+    const error = () => {
+      status.textContent = "Error your location"
+    }
+    navigator.geolocation.getCurrentPosition(success, error)
+  }
+
+  findMyLocation()
+  
+    function addZero(i) {
+      if (i < 10) {i = "0" + i}
+      return i;
+    }
+  
+    const TimeLocation = () => {
+        const d = new Date();
+        let h = addZero(d.getHours());
+        let m = addZero(d.getMinutes());
+        let s = addZero(d.getSeconds());
+        let time = h + ":" + m ;
+        return time
+      }
+
+  const onClickButton = (modal) => {
+    setOpenModal({
+      ...openModal,
+      [modal]: !openModal[modal],
+    });
+  };
+
+  //Cifrar mensajes
+  const cifrar = (mensaje, id) => {
+    const textoCifrado = CryptoJS.AES.encrypt(mensaje, id).toString()
+    return textoCifrado
+  }
+//Descrifrar mensajes
+  const descifrar = (mensaje, id) => {
+    const bytes = CryptoJS.AES.decrypt(mensaje, id)
+    const textoDescifrado = bytes.toString(CryptoJS.enc.Utf8)
+    return textoDescifrado
+  }
+
+
+  
+
+
+  //Actualizar usuarios bloqueados
+  const updateBlockedUsers = async (currentUserId, lista) => {
+    const userRef = doc(db, "users", currentUserId);
+    const user = await getDoc(userRef);
+    const userData = user.data()
+    const obj = {
+      ...userData,
+      ['blockedUsers']: lista
+    } 
+    await setDoc(userRef, obj);
+  }
+
+  //Funciones firebase
+  const updateMsg = async (path, docId, obj, collectionId) => {
+    await setDoc(doc(db, path, docId), obj);
+
+    const msgRef = doc(db, "lastMsg", collectionId);
+    const lastMsg = await getDoc(msgRef);
+    if (lastMsg.data()) {
+      if (lastMsg.data().id === docId) {
+        try {
+          await setDoc(msgRef, obj);
+        } catch {}
+      }
+    }
+  };
+
+  const deleteMsg = async (path, docId, collectionId) => {
+    const document = doc(db, path, docId);
+    await deleteDoc(document);
+
+    const msgRef = doc(db, "lastMsg", collectionId);
+    const lastMsg = await getDoc(msgRef);
+    if (lastMsg.data()) {
+      if (lastMsg.data().id === docId) {
+        try {
+          await deleteDoc(msgRef);
+        } catch {}
+      }
+    }
+  };
+
+  //Buscar mensaje
+  let mensajesBuscados = [];
+  
+  if (textoBuscado === '') {
+    mensajesBuscados = [...msgs]
+  } else {
+    mensajesBuscados = msgs.filter(m => {
+      const msgtext = descifrar(m.text, m.id).toLowerCase();
+      let nombreArchivo = ''
+      if (m.media) {
+        nombreArchivo = m.media.nombre.toLowerCase();
+      }
+      const texto = textoBuscado.toLowerCase();
+
+      return msgtext.includes(texto) || nombreArchivo.includes(texto);
+    })
+  }
+
+//Buscar mensaje en grupo
+let mensajesBuscadosG = [];
+  
+  if (textoBuscadoG === '') {
+    mensajesBuscadosG = [...msgG]
+  } else {
+    mensajesBuscadosG = msgG.filter(m => {
+      const msgtextG = descifrar(m.text, m.id).toLowerCase();
+      let nombreArchivo = ''
+      if (m.media) {
+        nombreArchivo = m.media.nombre.toLowerCase();
+      }
+      const textoG = textoBuscadoG.toLowerCase();
+      return msgtextG.includes(textoG) || nombreArchivo.includes(textoG);
+    })
+  }
+
+  const handleSearchGIFSelected = (selected) => {
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=FfC0GoBNWauajLfhAlvJ7Yc2ebHobpyu&q=${selected}&limit=25&offset=0&rating=g&lang=en`).then((response) => {
+      setGIF(response.data.data);
+      (openDialog.showGIF) ? setOpenDialog({...openDialog, showGIF: false}) :  setOpenDialog({...openDialog, showGIF: true})
+    });
+  }
+
+
+
+  return (
+    <appContext.Provider
+      value={{
+        openModal,
+        onClickButton,
+        handleSearchGIFSelected,
+        GIF, setGIF,
+        openDialog,
+        openAlert,
+        setOpenAlert,
+        handleOpenWeatherCommant,
+        handleWeather,
+        setOpenDialog,
+        updateMsg,
+        deleteMsg,
+        openAlert, 
+        setOpenAlert,
+        selectedMsg,
+        weather, 
+        selectedGIF, 
+        setSelectedGIF,
+        selectGIF, 
+        setSelectGIF,
+        setWeather,
+        data,
+        setData,
+        setSelectedMsg,
+        reminder, 
+        TimeLocation,
+        setReminder,
+        selected,
+        setSelected,
+        msgs,
+        setMsgs,
+        textoBuscado,
+        textoBuscadoG,
+        setTextoBuscado,
+        setTextoBuscadoG,
+        mensajesBuscados,
+        mensajesBuscadosG,
+        blockedUsers,
+        setBlockedUsers,
+        updateBlockedUsers,
+        msgG,
+        setMsgG,
+        Me, 
+        setMe,
+        cifrar,
+        location,
+        findMyLocation,
+        descifrar
+      }}
+    >
+      {children}
+    </appContext.Provider>
+  );
+};
+
+export { AppContextProvider, appContext };
